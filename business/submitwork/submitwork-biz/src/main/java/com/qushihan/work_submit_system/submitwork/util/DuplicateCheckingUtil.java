dfb9f72511e57d6d606d5e818c02a689fe81cfa2
JimKing <1104585465@qq.com>
2019-05-17 11:41:38 +0800

    [fix]:修改算法样式，符合阿里代码规范

--- business/submitwork/submitwork-biz/src/main/java/com/qushihan/work_submit_system/submitwork/util/DuplicateCheckingUtil.java
+++ business/submitwork/submitwork-biz/src/main/java/com/qushihan/work_submit_system/submitwork/util/DuplicateCheckingUtil.java
@@ -148,14 +148,21 @@ public class DuplicateCheckingUtil {
     }
 
     /**
-     * 计算矢量距离
-     * Levenshtein Distance(LD)
-     * @param str1 str1
-     * @param str2 str2
-     * @return ld
+     * Levenshtein distance最先是由俄国科学家Vladimir Levenshtein在1965年发明，用他的名字命名。不会拼读，可以叫它edit distance（编辑距离）。
+     * Levenshtein distance可以用来：
+     * Spell checking(拼写检查)
+     * Speech recognition(语句识别)
+     * DNA analysis(DNA分析)
+     * Plagiarism detection(抄袭检测)
+     * LD用m*n的矩阵存储距离值。算法大概过程：
+     * str1或str2的长度为0返回另一个字符串的长度。
+     * 初始化(n+1)*(m+1)的矩阵d，并让第一行和列的值从0开始增长。
+     * 扫描两字符串（n*m级的），如果：str1[i] == str2[j]，用temp记录它，为0。否则temp记为1。然后在矩阵d[i][j]赋于d[i-1][j]+1 、d[i][j-1]+1、d[i-1][j-1]+temp三者的最小值。
+     * 扫描完后，返回矩阵的最后一个值即d[n][m]      * @param str1
+     * @param str2
+     * @return
      */
-    private int ld(String str1, String str2)
-    {
+    private int ld(String str1, String str2) {
         //Distance
         int [][] d;
         int n = str1.length();
@@ -165,70 +172,60 @@ public class DuplicateCheckingUtil {
         char ch1; //str1
         char ch2; //str2
         int temp;
-        if (n == 0)
-        {
+        if (n == 0) {
             return m;
         }
-        if (m == 0)
-        {
+        if (m == 0) {
             return n;
         }
         d = new int[n + 1][m + 1];
-        for (i = 0; i <= n; i++)
-        {   d[i][0] = i;
+        for (i = 0; i <= n; i++) {
+            d[i][0] = i;
         }
-        for (j = 0; j <= m; j++)
-        {
+        for (j = 0; j <= m; j++) {
             d[0][j] = j;
         }
-        for (i = 1; i <= n; i++)
-        {
+        for (i = 1; i <= n; i++) {
             ch1 = str1.charAt(i - 1);
             //match str2
             for (j = 1; j <= m; j++)
             {
                 ch2 = str2.charAt(j - 1);
-                if (ch1 == ch2)
-                {
+                if (ch1 == ch2) {
                     temp = 0;
                 }
-                else
-                {
+                else {
                     temp = 1;
                 }
-
                 d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + temp);
             }
         }
         return d[n][m];
     }
 
-    private int min(int one, int two, int three)
-    {
+    private int min(int one, int two, int three) {
         int min = one;
-        if (two < min)
-        {
+        if (two < min) {
             min = two;
         }
-        if (three < min)
-        {
+        if (three < min) {
             min = three;
         }
         return min;
     }
 
     /**
-     * 删除char[]数组中_start位置到_end位置的元素
+     * 删除char[]数组中start位置到end位置的元素
      *
-     * @param _target
-     * @param _start
-     * @param _end
+     * @param target
+     * @param start
+     * @param end
      * @return
      */
-    private char[] del(char[] _target, int _start, int _end) {
-        char[] tmp = new char[_target.length - (_end - _start + 1)];
-        System.arraycopy(_target, 0, tmp, 0, _start);
-        System.arraycopy(_target, _end + 1, tmp, _start, _target.length - _end
+    private char[] del(char[] target, int start, int end) {
+        char[] tmp = new char[target.length - (end - start + 1)];
+        System.arraycopy(target, 0, tmp, 0, start);
+        System.arraycopy(target, end + 1, tmp, start, target.length - end
                 - 1);
         return tmp;
     }
@@ -236,14 +233,14 @@ public class DuplicateCheckingUtil {
     /**
      * 删除代码中的注释
      *
-     * @param _target
+     * @param target
      * @return
      */
-    private String delComments(String _target) {
+    private String delComments(String target) {
         int preType = 0;
         int mark = -1, token = -1;
         // 输入字符串
-        char[] input =  _target.toCharArray();
+        char[] input =  target.toCharArray();
         for (int cur = 0; cur < input.length; cur++) {
             if (input[cur] == MARK) {
                 // 首先判断是否为转义引号
